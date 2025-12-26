84eaace37c51a7ff5b29b0740ddda770bb45d01a
Sarath Sai Sujith Srinivas Grandhe <sarathsai.grandhe@gmail.com>
2025-12-26 16:00:37 -0500

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -5,82 +5,44 @@ import CopyEmailButton from "../components/CopyEmailButton";
 import { Frameworks } from "../components/Frameworks";
 
 const About = () => {
-  const grid2Container = useRef(null);
+  const grid2Container = useRef();
 
   return (
     <section className="c-space section-spacing" id="about">
       <h2 className="text-heading">About Me</h2>
 
-      <div className="grid grid-cols-1 gap-4 mt-12 md:grid-cols-6 md:auto-rows-[18rem]">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12">
         {/* Grid 1 */}
         <div className="flex items-end grid-default-color grid-1">
           <img
             src="assets/coding-pov.png"
-            alt="Coding point of view"
             className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
+            alt="Coding POV"
           />
-
           <div className="z-10">
-            <p className="headtext">Hi, I&apos;m Sarath Sai Sujith Grandhe</p>
+            <p className="headtext">Hi, I'm Sarath Sai Sujith Grandhe</p>
             <p className="subtext">
-              Over the last five years, I’ve built strong data engineering and
-              analytics skills, designing end-to-end pipelines, cloud-based data
-              platforms, and analytical solutions that turn raw data into
-              actionable business insights.
+              Over the last five years, I’ve built strong data engineering and analytics skills, designing end-to-end pipelines,
+              cloud-based data platforms, and analytical solutions that turn raw data into actionable business insights.
             </p>
           </div>
-
           <div className="absolute inset-x-0 pointer-events-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
         </div>
 
         {/* Grid 2 */}
         <div className="grid-default-color grid-2">
-          <div
-            ref={grid2Container}
-            className="flex items-center justify-center w-full h-full"
-          >
+          <div ref={grid2Container} className="flex items-center justify-center w-full h-full">
             <p className="flex items-end text-5xl text-gray-500">CODE IS CRAFT</p>
 
-            <Card
-              style={{ rotate: "75deg", top: "30%", left: "20%" }}
-              text="GRASP"
-              containerRef={grid2Container}
-            />
-            <Card
-              style={{ rotate: "-30deg", top: "60%", left: "45%" }}
-              text="SOLID"
-              containerRef={grid2Container}
-            />
-            <Card
-              style={{ rotate: "90deg", bottom: "30%", left: "70%" }}
-              text="Design Patterns"
-              containerRef={grid2Container}
-            />
-            <Card
-              style={{ rotate: "-45deg", top: "55%", left: "0%" }}
-              text="Design Principles"
-              containerRef={grid2Container}
-            />
-            <Card
-              style={{ rotate: "20deg", top: "10%", left: "38%" }}
-              text="SRP"
-              containerRef={grid2Container}
-            />
-            <Card
-              style={{ rotate: "30deg", top: "70%", left: "70%" }}
-              image="assets/logos/csharp-pink.png"
-              containerRef={grid2Container}
-            />
-            <Card
-              style={{ rotate: "-45deg", top: "70%", left: "25%" }}
-              image="assets/logos/dotnet-pink.png"
-              containerRef={grid2Container}
-            />
-            <Card
-              style={{ rotate: "-45deg", top: "5%", left: "10%" }}
-              image="assets/logos/blazor-pink.png"
-              containerRef={grid2Container}
-            />
+            <Card style={{ rotate: "75deg", top: "30%", left: "20%" }} text="GRASP" containerRef={grid2Container} />
+            <Card style={{ rotate: "-30deg", top: "60%", left: "45%" }} text="SOLID" containerRef={grid2Container} />
+            <Card style={{ rotate: "90deg", bottom: "30%", left: "70%" }} text="Design Patterns" containerRef={grid2Container} />
+            <Card style={{ rotate: "-45deg", top: "55%", left: "0%" }} text="Design Principles" containerRef={grid2Container} />
+            <Card style={{ rotate: "20deg", top: "10%", left: "38%" }} text="SRP" containerRef={grid2Container} />
+
+            <Card style={{ rotate: "30deg", top: "70%", left: "70%" }} image="assets/logos/csharp-pink.png" containerRef={grid2Container} />
+            <Card style={{ rotate: "-45deg", top: "70%", left: "25%" }} image="assets/logos/dotnet-pink.png" containerRef={grid2Container} />
+            <Card style={{ rotate: "-45deg", top: "5%", left: "10%" }} image="assets/logos/blazor-pink.png" containerRef={grid2Container} />
           </div>
         </div>
 
@@ -88,11 +50,8 @@ const About = () => {
         <div className="grid-black-color grid-3">
           <div className="z-10 w-[50%]">
             <p className="headtext">Time Zone</p>
-            <p className="subtext">
-              I&apos;m based in Mars, and open to remote work worldwide
-            </p>
+            <p className="subtext">I'm based in Cincinnati, and open to remote work worldwide</p>
           </div>
-
           <figure className="absolute left-[30%] top-[10%]">
             <Globe />
           </figure>
@@ -101,9 +60,7 @@ const About = () => {
         {/* Grid 4 */}
         <div className="grid-special-color grid-4">
           <div className="flex flex-col items-center justify-center gap-4 size-full">
-            <p className="text-center headtext">
-              Do you want to start a project together?
-            </p>
+            <p className="text-center headtext">Do you want to start a project together?</p>
             <CopyEmailButton />
           </div>
         </div>
@@ -113,13 +70,12 @@ const About = () => {
           <div className="z-10 w-[50%]">
             <p className="headtext">Tech Stack</p>
             <p className="subtext">
-              I specialize in modern data and analytics tools, leveraging cloud
-              platforms, programming languages, and frameworks to build robust,
+              I specialize in modern data and analytics tools, leveraging cloud platforms, programming languages, and frameworks to build robust,
               scalable data pipelines and insight-driven solutions.
             </p>
           </div>
 
-          <div className="absolute inset-y-0 w-full h-full md:inset-y-9 start-[50%] md:scale-125">
+          <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale-125">
             <Frameworks />
           </div>
         </div>
