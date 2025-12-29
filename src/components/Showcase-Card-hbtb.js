ffc30bc88371a094c8e65770eedc1753921dea11
Sarath Sai Sujith Srinivas Grandhe <sarathsai.grandhe@gmail.com>
2025-12-28 20:51:24 -0500

    Delete src/components/Showcase-Card-hbtb.js

--- src/components/Showcase-Card-hbtb.js
+++
@@ -1,8 +0,0 @@
-/*
- * Showcase Card
- *
- * Learn More: https://www.framer.com/asset-urls
- */
-
-export * from "https://framerusercontent.com/modules/i2b5FD5cAk5nwNCx7gIh/baT94z3cbRmShOSZjllW/Nh9unIpFh.js";
-export { default } from "https://framerusercontent.com/modules/i2b5FD5cAk5nwNCx7gIh/baT94z3cbRmShOSZjllW/Nh9unIpFh.js";
